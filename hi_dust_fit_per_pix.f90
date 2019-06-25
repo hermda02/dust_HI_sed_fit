8c83f447fe6d748403a8933c458c3d3ffbadef74
hermda02 <hermda02@luther.edu>
2019-06-25 11:35:08 +0200

    new chisq test

--- hi_dust_fit_per_pix.f90
+++ hi_dust_fit_per_pix.f90
@@ -380,18 +380,20 @@ contains
 
     chi = 0.d0
     do j=1,bands
-       do i=0,50
+       do i=0,15
           if (abs((HI(i,1)-missval)/missval) < 1.d-8) then
              cycle
           else
              model(i,1,j) = HI(i,1)*planck(freq(j)*1.d9,T(i))
+             write(*,*) model(i,1,j)
              chi = chi + (maps(i,1,j) - amp(i,j)*model(i,1,j))**2.d0/cov(i,1,j)
              write(*,*) (maps(i,1,j) - amp(i,j)*model(i,1,j))
              write(*,*) (maps(i,1,j) - amp(i,j)*model(i,1,j))**2.d0
              write(*,*) (maps(i,1,j) - amp(i,j)*model(i,1,j))**2.d0/cov(i,1,j)
-             write(*,*) chi
+!             write(*,*) chi
           end if
        end do
+       stop
     end do
 
     compute_chisq = (chi/(pix*bands))
